f844322a8197c83d9c9c028e4c2151c2d8e477b7
Vicente Neto <sneto.vicente@gmail.com>
2016-11-06 11:01:18 -0300

    Replacing @EnableDiscoveryClient by @EnableEurekaClient

--- src/main/java/br/com/vneto/ServiceInstanceRestController.java
+++ src/main/java/br/com/vneto/ServiceInstanceRestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServiceInstanceRestController {
+
 	@Autowired
 	private DiscoveryClient discoveryClient;
 
